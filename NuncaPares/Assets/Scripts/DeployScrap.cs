ddd3c40df2497943a39b80a52f06f69cf208e918
CCando <carloscando12@hotmail.com>
2020-11-25 20:09:56 -0500

    generacion de tuercas y tornillos

---
+++ NuncaPares/Assets/Scripts/DeployScrap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployScrap : MonoBehaviour
+{
+    public GameObject nutPrefab;
+    public GameObject gearPrefab;
+    public float respawnTime = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(scrapWave());
+    }
+
+    private void spawnScrap()
+    {
+        GameObject nut = Instantiate(nutPrefab) as GameObject;
+        GameObject gear = Instantiate(gearPrefab) as GameObject;
+        nut.transform.position = new Vector2(Random.Range(-12, 12), 8);
+        gear.transform.position = new Vector2(Random.Range(-12, 12), 15);
+    }
+
+    IEnumerator scrapWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnScrap();
+        }
+    }
+}
---
+++ NuncaPares/Assets/Scripts/ScrapSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapSpawn : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y  < -20.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
