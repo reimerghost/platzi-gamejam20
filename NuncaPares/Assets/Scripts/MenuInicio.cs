47470234cfc2d7c87d6631d029943ae4a0cd823c
Reimer Chamalé <rchamale10@gmail.com>
2020-11-25 18:35:53 -0600

    Menú inicial básico

---
+++ NuncaPares/Assets/Scripts/MenuInicio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicio : MonoBehaviour
+{
+    public void cargarEscena(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
