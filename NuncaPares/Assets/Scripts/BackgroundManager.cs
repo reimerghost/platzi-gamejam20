0a52168a782e67f22b7e8d9a12a0837898702a61
Reimer Chamalé <rchamale10@gmail.com>
2020-11-25 19:25:04 -0600

    Parallax vertical

--- NuncaPares/Assets/Scripts/BackgroundManager.cs
+++ NuncaPares/Assets/Scripts/BackgroundManager.cs
@@ -5,33 +5,64 @@ using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
+    public float scrollSpeed;
+    
     public List<GameObject> fondos;
-    private GameObject actual, siguiente;
+    private GameObject actual, siguiente, anterior;
+    
+    private int index = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        actual = fondos[0];
-        siguiente = fondos[1];
-        
+        actual = fondos[index];
         // Instanciar gameObjects iniciales
         actual = Instantiate (actual, new Vector3(0,0,9.11f), Quaternion.identity) as GameObject;
         actual.transform.SetParent (GetComponent<Transform>());
+        actual.name = "bloque_" + index;
+        index++;
+        
+        siguiente = fondos[index];
         siguiente = Instantiate (siguiente, new Vector3(0,18,9.11f), Quaternion.identity) as GameObject;
         siguiente.transform.SetParent (GetComponent<Transform>());
+        siguiente.name = "bloque_" + index;
+        index++;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-            moverFondos();
+        MoverFondos();
     }
 
-    void moverFondos()
+
+    private float desp;
+    private float distancia;
+    
+    void MoverFondos()
     {
-        actual.transform.Translate(0,-5*Time.deltaTime,0);
-        siguiente.transform.Translate(0,-5*Time.deltaTime,0);
+        desp = -scrollSpeed * Time.deltaTime;
+        distancia += desp;
+        
+        actual.transform.Translate(0,desp,0);
+        siguiente.transform.Translate(0,desp,0);
+
+        if ((distancia > -18f)) return;
+        anterior = actual;
+        actual = siguiente;
+        
+        siguiente = fondos[index];
+        siguiente = Instantiate (siguiente, new Vector3(0,18,9.11f), Quaternion.identity) as GameObject;
+        siguiente.transform.SetParent (GetComponent<Transform>());
+        siguiente.name = "bloque_" + index;
+        index++;
+        if (index>=fondos.Count-1)
+        {
+            index = 0;
+        }
+        Destroy(anterior);
+        distancia = 0;
     }
     
     
