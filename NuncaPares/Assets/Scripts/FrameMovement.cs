78a4bb54da6ffb48654764fa2d8b5dfe86e44d61
CCando <carloscando12@hotmail.com>
2020-11-25 21:00:56 -0500

    Cambio de marco y correción de movimiento

--- NuncaPares/Assets/Scripts/FrameMovement.cs
+++ NuncaPares/Assets/Scripts/FrameMovement.cs
@@ -6,6 +6,7 @@ public class FrameMovement : MonoBehaviour
 {
     public float speed = 5.0f;
     float timeRemaining = 60.0f;
+    float timeMove = 2.0f;
     bool moveUp = false;
     bool moveRight = false;
     bool timerIsRunning = false;
@@ -28,23 +29,30 @@ public class FrameMovement : MonoBehaviour
             {
                 timeRemaining -= Time.deltaTime;
 
-                if (moveUp)
+                while(timeMove > 0)
                 {
-                    transform.position = new Vector3(Mathf.Clamp(transform.position.x + (speed * Time.deltaTime), -6.0f, 6.0f), transform.position.y, transform.position.z);
-                }
-                else
-                {
-                    transform.position = new Vector3(Mathf.Clamp(transform.position.x - (speed * Time.deltaTime), -6.0f, 6.0f), transform.position.y, transform.position.z);
+                    if (moveUp)
+                    {
+                        transform.position = new Vector3(Mathf.Clamp(transform.position.x + (speed * Time.deltaTime), -6.0f, 6.0f), transform.position.y, transform.position.z);
+                    }
+                    else
+                    {
+                        transform.position = new Vector3(Mathf.Clamp(transform.position.x - (speed * Time.deltaTime), -6.0f, 6.0f), transform.position.y, transform.position.z);
+                    }
+
+                    if (moveRight)
+                    {
+                        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + (speed * Time.deltaTime), -3.8f, 3.8f), transform.position.z);
+                    }
+                    else
+                    {
+                        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y - (speed * Time.deltaTime), -3.8f, 3.8f), transform.position.z);
+                    }
+
+                    timeMove -= Time.deltaTime;
                 }
 
-                if (moveRight)
-                {
-                    transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y + (speed * Time.deltaTime), -3.8f, 3.8f), transform.position.z);
-                }
-                else
-                {
-                    transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y - (speed * Time.deltaTime), -3.8f, 3.8f), transform.position.z);
-                }
+                timeMove = 2.0f;
 
                 //transform.position = new Vector3(Mathf.Clamp(Input.GetAxis("Horizontal"), -8.0f, 8.0f), Mathf.Clamp(Input.GetAxis("Vertical"), -4.8f, 4.8f), transform.position.z);
             } else
